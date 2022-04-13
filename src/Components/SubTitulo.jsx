75ab8fd245620242e42eb144169ff04168f346d0
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-04-13 12:43:17 -0300

    atualizando imgs

--- src/Components/SubTitulo.jsx
+++ src/Components/SubTitulo.jsx
@@ -4,7 +4,7 @@ const SubTitulo = (props) => {
   return (
     <div className="text-center">
       <h2 style={{fontSize:32}}>Venda: Lojas/Casas</h2>
-      <ButtonCatalogo link="https://equiplan.orbitare.com.br/imoveis?deal_type=1&tipos%5B%5D=22&tipos%5B%5D=19"/>
+      <ButtonCatalogo link="https://equiplan.orbitare.com.br"/>
       </div>
   );
 };
--- src/Components/Titulo.jsx
+++ src/Components/Titulo.jsx
@@ -6,7 +6,7 @@ const Titulo = (props) => {
   return (
       <div className="text-center">
         <h1 style={{fontSize:32}}>Locação: Lojas/Casas</h1>
-        <ButtonCatalogo link="https://equiplan.orbitare.com.br/imoveis?deal_type=1&tipos%5B%5D=9&tipos%5B%5D=1004&tipos%5B%5D=22"/>
+        <ButtonCatalogo link="https://equiplan.orbitare.com.br"/>
       </div>
   );
 };
