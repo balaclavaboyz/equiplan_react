317e5057c76d1171a80ab9a91d73f472c3e0acc8
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-04-28 12:50:25 -0300

    atualizando imgs

--- src/Components/Titulo.jsx
+++ src/Components/Titulo.jsx
@@ -5,7 +5,7 @@ import "../assets/my.css";
 const Titulo = (props) => {
   return (
       <div className="text-center">
-        <h1 style={{fontSize:32}}>Locação:</h1>
+        <h1 style={{fontSize:32}}>Confira!:</h1>
         <ButtonCatalogo link="https://equiplan.orbitare.com.br"/>
       </div>
   );
