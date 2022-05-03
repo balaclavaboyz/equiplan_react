31e471731d0f68ee8622277038b15dcdf724f9d1
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-05-03 10:16:15 -0300

    atualizando imgs

--- src/Components/missao.jsx
+++ src/Components/missao.jsx
@@ -8,7 +8,7 @@ const Faq = () => {
       <h1>Missão - Visão - Valor</h1>
       <br></br>
       <Row>
-        <h2>Criar, Produzir e Multiplicar Oportunidades</h2>
+        <h2>Criar, Realizar e Multiplicar Oportunidades</h2>
         <p>Whatsapp: <a href="https://wa.me/5511973364612">(11)97336-4612</a></p>
         <p>E-mail: <a href="mailto:equiplan@equiplan.com.br">equiplan@equiplan.com.br</a> </p>
         <p>Contato telefonico: <a href="tel:11902809773">(11)90280-9773</a></p>
