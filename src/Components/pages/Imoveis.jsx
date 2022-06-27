e15ae812071295f1af9aba1b50552fff8d14f62a
peter <peterthomaskim@gmail.com>
2022-06-27 14:46:26 -0300

    prototype done for imovel descp

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -2,57 +2,70 @@ import React from "react";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
 import IM79703 from "./imoveis/IM79703";
 import StraightenIcon from "@mui/icons-material/Straighten";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import DescriptionIcon from "@mui/icons-material/Description";
+import SensorDoorOutlinedIcon from "@mui/icons-material/SensorDoorOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import InnerHTML from "dangerously-set-html-content";
+import RouteOutlinedIcon from "@mui/icons-material/RouteOutlined";
 
 export default function Imoveis() {
-    return (
-        <>
-            <div className="container_imoveis_menu container">
-                <div className="imoveis_menu">
-                    <h1>Índice</h1>
-                    <button>
-                        <Link to="IM79703">im79703</Link>
-                    </button>
-                </div>
-                <div className="imoveis_context">
-                    <div className="imovelGaleria">
-                        <Routes>
-                            <Route path="IM79703" element={<IM79703 />} />
-                        </Routes>
-                        <Outlet />
-                    </div>
-                </div>
-                <div className="imoveis_desp">
-                    {/*titulo  */}
-                    <h1>Casa em Lapa com 200m²</h1>
-                    {/* endereco */}
-                    <p>Rua Guararapes, 669, Lapa, São Paulo / SP</p>
-                    {/* preco */}
-                    <p>Aluguel R$ 4.000,00</p>
-                    {/* botao para contato ou proposta, talvez email */}
-                    {/* icons emoji com info */}
-                    <div className="imoveis_specs">
-                        <div className="imoveis_specs_pos">
-                            <StraightenIcon></StraightenIcon>
-                            200m²
-
-                            1 suites
-
-                            1 vaga
-                            5 quartos
-                            
-                        {/* metro quadrado, quantos quartos, banheiros, vaga de carro, andar do ape, mobilias e prox do metro */}
-                        </div>
-                    </div>
-                    {/* descripcao do imovel */}
-                    <p>
-                        Parte inferior: 3 dormitórios (suíte), banheiro, sala
-                        jantar, sala social Parte superior: 2 dormitórios, sala,
-                        cozinha, banheiro, garagem, edícula e quintal
-                        IPTU:.R$1.100,00 p/mês
-                    </p>
-                    <p>CEP: 5077051</p>
-                </div>
+  const googleMaps = `
+  <div class="mapouter"><div class="gmap_canvas"><iframe src="https://maps.google.com/maps?q=rua%20guararapes%20669%20lapa&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas" frameborder="0" scrolling="no" style="width: 600px; height: 400px;"></iframe><style>.mapouter{position:relative;text-align:right;height:400px;width:600px;}</style><a href="https://googlemapsiframegenerator.com" style="display:none">Google Maps Iframe Generator - Free Html Embed Code</a><style>.gmap_canvas{overflow:hidden;background:none!important;height:400px;width:600px;}</style></div></div>
+  `;
+  return (
+    <>
+      <div className="container_imoveis_menu container">
+        <div className="imoveis_menu">
+          <h1>Índice</h1>
+          <button>
+            <Link to="IM79703">im79703</Link>
+          </button>
+        </div>
+        <div className="imoveis_context">
+          <div className="imovelGaleria">
+            <Routes>
+              <Route path="IM79703" element={<IM79703 />} />
+            </Routes>
+            <Outlet />
+          </div>
+        </div>
+        <div className="imoveis_desp">
+          <hr />
+          {/*titulo  */}
+          <h1>Casa em Lapa com 200m²</h1>
+          {/* endereco */}
+          <p>Rua Guararapes, 669, Lapa, São Paulo / SP - 5077051</p>
+          {/* preco */}
+          <p>Aluguel R$ 4.000,00</p>
+          {/* botao para contato ou proposta, talvez email */}
+          <button>Entrar em contato</button>
+          {/* icons emoji com info */}
+          <div className="imoveis_specs">
+            <div className="imoveis_specs_pos">
+              <StraightenIcon></StraightenIcon>
+              200m²
             </div>
-        </>
-    );
+            <div className="imoveis_specs_pos">
+              <SensorDoorOutlinedIcon></SensorDoorOutlinedIcon>1 suite 5 quartos
+            </div>
+            <div className="imoveis_specs_pos">
+              <DirectionsCarIcon></DirectionsCarIcon>1 vaga
+            </div>
+            {/* metro quadrado, quantos quartos, banheiros, vaga de carro, andar do ape, mobilias e prox do metro */}
+          </div>
+          {/* descripcao do imovel */}
+          <hr />
+          <h2>Descrição</h2>
+          <p>
+            Parte inferior: 3 dormitórios (suíte), banheiro, sala jantar, sala
+            social Parte superior: 2 dormitórios, sala, cozinha, banheiro,
+            garagem, edícula e quintal IPTU:.R$1.100,00 p/mês
+          </p>
+          <hr />
+          <InnerHTML className="center" html={googleMaps}></InnerHTML>
+        </div>
+      </div>
+    </>
+  );
 }
