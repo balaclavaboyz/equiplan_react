7a774fb60b74ca9d8df16ea310e846c140272b59
peter <peterthomaskim@gmail.com>
2022-06-27 13:01:47 -0300

    descp comments

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -22,10 +22,28 @@ export default function Imoveis() {
                     </div>
                 </div>
                 <div className="imoveis_desp">
+                    {/*titulo  */}
                     <h1>Casa em Lapa com 200m²</h1>
-                    <p>Lapa</p>
-                    <p>Rua Guararapes, 669 - São Paulo / SP</p>
+                    {/* endereco */}
+                    <p>Rua Guararapes, 669, Lapa, São Paulo / SP</p>
+                    {/* preco */}
                     <p>Aluguel R$ 4.000,00</p>
+                    {/* botao para contato ou proposta, talvez email */}
+                    {/* icons emoji com info */}
+                    <div className="imoveis_specs">
+                        <div className="imoveis_specs_pos">
+                            <StraightenIcon></StraightenIcon>
+                            200m²
+
+                            1 suites
+
+                            1 vaga
+                            5 quartos
+                            
+                        {/* metro quadrado, quantos quartos, banheiros, vaga de carro, andar do ape, mobilias e prox do metro */}
+                        </div>
+                    </div>
+                    {/* descripcao do imovel */}
                     <p>
                         Parte inferior: 3 dormitórios (suíte), banheiro, sala
                         jantar, sala social Parte superior: 2 dormitórios, sala,
@@ -33,13 +51,6 @@ export default function Imoveis() {
                         IPTU:.R$1.100,00 p/mês
                     </p>
                     <p>CEP: 5077051</p>
-                    <div className="imoveis_specs">
-                        <div className="imoveis_specs_pos">
-                            <StraightenIcon></StraightenIcon>
-                            200m²
-                            {/* TODO fazer as legendas */}
-                        </div>
-                    </div>
                 </div>
             </div>
         </>
