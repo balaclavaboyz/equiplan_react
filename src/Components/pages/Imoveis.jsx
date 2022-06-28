3666ee44c10d2f457fabd13301c194e9ab839439
peter <peterthomaskim@gmail.com>
2022-06-28 11:52:31 -0300

    index page done

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -5,14 +5,11 @@ import IM79703 from "./imoveis/IM79703";
 export default function Imoveis() {
     return (
         <>
-            <div className="container_imoveis_menu container">
-                <div className="imoveis_menu">
-                    <h1>Índice</h1>
-                    <button>
-                        <Link to="/IM79703">im79703</Link>
-                    </button>
-                </div>
-            </div>
+        <div className="indice_imoveis_wrapper">
+            <h1>Índice de Imóveis</h1>
+
+            <Link to="/IM79703" >IM79703</Link>
+        </div>
         </>
     );
 }
