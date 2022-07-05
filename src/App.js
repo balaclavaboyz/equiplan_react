c84bc860b2609d2784ef931edf82dc6f6ea13269
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-07-05 15:49:28 -0300

    add nova page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Missao from "./Components/pages/missao";
 import WhatsappIcon from "./Components/WhatsappIcon";
 import Footer from "./Components/Footer";
 import Imoveis from "./Components/pages/Imoveis";
+import IM79939 from "./Components/pages/imoveis/IM79939";
 import IM79881 from "./Components/pages/imoveis/IM79881";
 import IM79886 from "./Components/pages/imoveis/IM79886";
 import IM79836 from "./Components/pages/imoveis/IM79836";
@@ -28,6 +29,7 @@ const App = () => {
                         <Route path="contato" element={<Contato />} />
                         <Route path="missao" element={<Missao />} />
                         <Route path="imoveis" element={<Imoveis />}></Route>
+                        <Route path="IM79939" element={<IM79939 />} />
                         <Route path="IM79881" element={<IM79881 />} />
                         <Route path="IM79886" element={<IM79886 />} />
                         <Route path="IM79836" element={<IM79836 />} />
