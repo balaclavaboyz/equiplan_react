3bc3a8249093d7eeef9cdd5547c72943949b9015
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-07-13 14:27:40 -0300

    add nova page

--- src/App.js
+++ src/App.js
@@ -9,15 +9,7 @@ import Missao from "./Components/pages/missao";
 import WhatsappIcon from "./Components/WhatsappIcon";
 import Footer from "./Components/Footer";
 import Imoveis from "./Components/pages/Imoveis";
-import IM80009 from "./Components/pages/imoveis/IM80009";
 import IM80036 from "./Components/pages/imoveis/IM80036";
-import IM80017 from "./Components/pages/imoveis/IM80017";
-import IM79981 from "./Components/pages/imoveis/IM79981";
-import IM79973 from "./Components/pages/imoveis/IM79973";
-import IM79941 from "./Components/pages/imoveis/IM79941";
-import IM79922 from "./Components/pages/imoveis/IM79922";
-import IM79866 from "./Components/pages/imoveis/IM79866";
-import IM79836 from "./Components/pages/imoveis/IM79836";
 
 const App = () => {
     return (
@@ -32,15 +24,7 @@ const App = () => {
                         <Route path="contato" element={<Contato />} />
                         <Route path="missao" element={<Missao />} />
                         <Route path="imoveis" element={<Imoveis />}></Route>
-                        <Route path="IM80009" element={<IM80009 />} />
                         <Route path="IM80036" element={<IM80036 />} />
-                        <Route path="IM80017" element={<IM80017 />} />
-                        <Route path="IM79981" element={<IM79981 />} />
-                        <Route path="IM79973" element={<IM79973 />} />
-                        <Route path="IM79941" element={<IM79941 />} />
-                        <Route path="IM79922" element={<IM79922 />} />
-                        <Route path="IM79866" element={<IM79866 />} />
-                        <Route path="IM79836" element={<IM79836 />} />
                     </Routes>
                 </div>
             </div>
--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -8,15 +8,7 @@ export default function Imoveis() {
 
             <div className="indice_imoveis_wrapper">
                 <h1>Casas / Lojas Comerciais</h1>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM80009">IM80009</Link><p>Casa em Brooklin Paulista com 92m²</p><p>Tipo: Venda</p><p>Valor Locação: R$ 0,00</p><p>Valor Venda: R$ 700.000,00</p></div>
                 <div style={{display:'flex',gap:'1rem'}}><Link to="/IM80036">IM80036</Link><p>Casa em Jardim Ipanema (Zona Sul) com 315m²</p><p>Tipo: Venda e Aluguel</p><p>Valor Locação: R$ 3.500,00</p><p>Valor Venda: R$ 1.100.000,00</p></div>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM80017">IM80017</Link><p>Casa em Cambuci com 180m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 6.300,00</p><p>Valor Venda: R$ 0,00</p></div>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79981">IM79981</Link><p>Casa em Vila da Saúde com 80m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 2.500,00</p><p>Valor Venda: R$ 0,00</p></div>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79973">IM79973</Link><p>Casa em Jardim Miriam com 282m²</p><p>Tipo: Venda e Aluguel</p><p>Valor Locação: R$ 5.000,00</p><p>Valor Venda: R$ 1.272.000,00</p></div>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79941">IM79941</Link><p>Salão Comercial em Vila Moraes com 40m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 1.600,00</p><p>Valor Venda: R$ 0,00</p></div>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79922">IM79922</Link><p>Loja em Lapa com 228m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 4.500,00</p><p>Valor Venda: R$ 0,00</p></div>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79866">IM79866</Link><p>Galpão em Vila Romana com 130m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 4.000,00</p><p>Valor Venda: R$ 0,00</p></div>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79836">IM79836</Link><p>Casa Comercial em Pinheiros com 400m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 4.000,00</p><p>Valor Venda: R$ 0,00</p></div>
             </div>
         </div>
         </>
