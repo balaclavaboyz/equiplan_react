1a685dd9df96d1e35655d404c2d3a1ea460b69e2
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-07-13 13:53:29 -0300

    add nova page

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -8,6 +8,7 @@ export default function Imoveis() {
 
             <div className="indice_imoveis_wrapper">
                 <h1>Casas / Lojas Comerciais</h1>
+                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM80036">IM80036</Link><p>Casa em Jardim Ipanema (Zona Sul) com 315m²</p><p>Tipo: Venda e Aluguel</p><p>Valor Locação: R$ 3.500,00</p><p>Valor Venda: R$ 1.100.000,00</p></div>
                 <div style={{display:'flex',gap:'1rem'}}><Link to="/IM80017">IM80017</Link><p>Casa em Cambuci com 180m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 6.300,00</p><p>Valor Venda: R$ 0,00</p></div>
                 <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79981">IM79981</Link><p>Casa em Vila da Saúde com 80m²</p><p>Tipo: Aluguel</p><p>Valor Locação: R$ 2.500,00</p><p>Valor Venda: R$ 0,00</p></div>
                 <div style={{display:'flex',gap:'1rem'}}><Link to="/IM79973">IM79973</Link><p>Casa em Jardim Miriam com 282m²</p><p>Tipo: Venda e Aluguel</p><p>Valor Locação: R$ 5.000,00</p><p>Valor Venda: R$ 1.272.000,00</p></div>
