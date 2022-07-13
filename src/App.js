937edce92adaa6b51820588a8294c1f3cc2cf4c3
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-07-13 16:40:47 -0300

    add nova page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Missao from "./Components/pages/missao";
 import WhatsappIcon from "./Components/WhatsappIcon";
 import Footer from "./Components/Footer";
 import Imoveis from "./Components/pages/Imoveis";
-import IM80036 from "./Components/pages/imoveis/IM80036";
 
 const App = () => {
     return (
@@ -24,7 +23,6 @@ const App = () => {
                         <Route path="contato" element={<Contato />} />
                         <Route path="missao" element={<Missao />} />
                         <Route path="imoveis" element={<Imoveis />}></Route>
-                        <Route path="IM80036" element={<IM80036 />} />
                     </Routes>
                 </div>
             </div>
--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -8,7 +8,6 @@ export default function Imoveis() {
 
             <div className="indice_imoveis_wrapper">
                 <h1>Casas / Lojas Comerciais</h1>
-                <div style={{display:'flex',gap:'1rem'}}><Link to="/IM80036">IM80036</Link><p>Casa em Jardim Ipanema (Zona Sul) com 315m²</p><p>Tipo: Venda e Aluguel</p><p>Valor Locação: R$ 3.500,00</p><p>Valor Venda: R$ 1.100.000,00</p></div>
             </div>
         </div>
         </>
