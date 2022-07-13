c5cfc0693bb9a7e1faa0fad13fcd840c6965574a
peter <peterthomaskim@gmail.com>
2022-07-13 12:37:27 -0300

    sibebar for mobile

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,88 @@
+import React,{useState} from "react";
+import {
+  ProSidebar,
+  Menu as MenuFromSidebar,
+  MenuItem,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+import { FaList, FaRegHeart } from "react-icons/fa";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { RiPencilLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
+import "react-pro-sidebar/dist/css/styles.css";
+import Img from "react-cool-img";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { Nav, Container, Row, Col, } from "react-bootstrap";
+import {slide as Burger } from 'react-burger-menu'
+
+const Sidebar = () => {
+  return (
+    <>
+      <Burger>
+        <Link to='/'>Home</Link>
+        <Link to='/imoveis'>Imóveis</Link>
+        <Link to='/faq'>Faq</Link>
+      </Burger>
+    </>
+    // <>
+    //   <Container className="mt-5">
+    //     <Row id="menu">
+    //       <Col md="auto">
+    //         <Link
+    //           to="/"
+    //           style={{ color: "inherit", textDecoration: "inherit" }}
+    //         >
+    //           <Img src={logo} alt="logo_milvest" width="100px" />
+    //         </Link>
+    //       </Col>
+    //       <Col>
+    //         <Nav className="nav">
+    //           <Nav.Item>
+    //             <Nav.Link as={Link} to="/" style={{ fontSize: "1rem",color:"white",fontWeight:"bold" }}>
+    //               HOME
+    //             </Nav.Link>
+    //           </Nav.Item>
+    //           <Nav.Item>
+    //             <Nav.Link as={Link} to="imoveis" style={{ fontSize: "1rem",color:"white",fontWeight:"bold" }}>
+    //               IMÓVEIS
+    //             </Nav.Link>
+    //           </Nav.Item>
+    //           <Nav.Item>
+    //             <Nav.Link as={Link} to="faq" style={{ fontSize: "1rem",color:"white",fontWeight:"bold" }}>
+    //               FAQ
+    //             </Nav.Link>
+    //           </Nav.Item>
+    //           <Nav.Item>
+    //             <Nav.Link as={Link} to="contato" style={{ fontSize: "1rem",color:"white",fontWeight:"bold" }}>
+    //               CONTATO
+    //             </Nav.Link>
+    //           </Nav.Item>
+    //           <Nav.Item>
+    //             <Nav.Link as={Link} to="missao" style={{ fontSize: "1rem",color:"white",fontWeight:"bold" }}>
+    //               MISSÃO
+    //             </Nav.Link>
+    //           </Nav.Item>
+    //           <Nav.Item>
+    //             <Nav.Link as={Link} to="politica" style={{ fontSize: "1rem",color:"white",fontWeight:"bold" }}>
+    //               POLÍTICA DE PRIVACIDADE
+    //             </Nav.Link>
+    //           </Nav.Item>
+    //         </Nav>
+    //       </Col>
+    //     </Row>
+    //     <Row>
+    //       <div id="header">
+    //         <div id="creci">
+    //           <p><b>CRECI-J35272</b></p>
+    //         </div>
+    //       </div>
+    //     </Row>
+    //   </Container>
+    // </>
+  );
+};
+
+export default Sidebar;
