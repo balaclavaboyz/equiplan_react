566fa228f9a95742250cdb5bcbf55e5191fd3e55
peter <peterthomaskim@gmail.com>
2022-07-14 13:57:27 -0300

    fixed table imoveis.jsx

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -1,29 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../../assets/table.css'
+import "../../assets/table.css";
+import "../../assets/table.js";
 
 export default function Imoveis() {
     return (
         <>
-        <div className="container">
-            <div className="indice_imoveis_wrapper">
-                <h1>Casas / Lojas Comerciais</h1>
-                <table className="rtable rtable--flip">
-                    <thead>
-                        <tr>
-                            <th>Código</th>
-                            <th>Título</th>
-                            <th>Tipo</th>
-                            <th>Locação</th>
-                            <th>Venda</th>
-                        </tr>
-                        <span id="insertion_table"></span>
-<tr id="IM79981"><th><Link to="/IM79981">IM79981</Link></th><th>Casa em Vila da Saúde com 80m²</th><th>Aluguel</th><th>R$ 2.500,00</th><th>R$ 0,00</th></tr>
-<tr id="IM80018"><th><Link to="/IM80018">IM80018</Link></th><th>Loja em Santo Amaro com 440m²</th><th>Aluguel</th><th>R$ 35.000,00</th><th>R$ 0,00</th></tr>
-                    </thead>
-                </table>
+            <div className="container">
+                <div className="indice_imoveis_wrapper">
+                    <h1>Casas / Lojas Comerciais</h1>
+                    <table className="rtable rtable--flip">
+                        <thead>
+                            <tr>
+                                <th>Código</th>
+                                <th>Título</th>
+                                <th>Tipo</th>
+                                <th>Locação</th>
+                                <th>Venda</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <span id="insertion_table"></span>
+                            <tr id="IM79981">
+                                <th>
+                                    <Link to="/IM79981">IM79981</Link>
+                                </th>
+                                <th>Casa em Vila da Saúde com 80m²</th>
+                                <th>Aluguel</th>
+                                <th>R$ 2.500,00</th>
+                                <th>R$ 0,00</th>
+                            </tr>
+                            <tr id="IM80018">
+                                <th>
+                                    <Link to="/IM80018">IM80018</Link>
+                                </th>
+                                <th>Loja em Santo Amaro com 440m²</th>
+                                <th>Aluguel</th>
+                                <th>R$ 35.000,00</th>
+                                <th>R$ 0,00</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         </>
     );
 }
---
+++ src/assets/table.js
@@ -0,0 +1,5 @@
+if (/(iPhone|iPad|iPod)/gi.test(navigator.userAgent) && window.location.pathname.indexOf('/full') > -1) {
+  var p = document.createElement('p');
+  p.innerHTML = '<a target="_blank" href="https://s.codepen.io/dbushell/debug/wGaamR"><b>Click here to view this demo properly on iOS devices (remove the top frame)</b></a>';
+  document.body.insertBefore(p, document.body.querySelector('h1'));
+}
