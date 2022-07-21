066b8723a62553629a0d41db52854142fa880660
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-07-21 15:06:41 -0300

    add nova page

--- src/App.js
+++ src/App.js
@@ -9,13 +9,6 @@ import Missao from "./Components/pages/missao";
 import WhatsappIcon from "./Components/WhatsappIcon";
 import Footer from "./Components/Footer";
 import Imoveis from "./Components/pages/Imoveis";
-import IM80058 from "./Components/pages/imoveis/IM80058";
-import IM80109 from "./Components/pages/imoveis/IM80109";
-import IM80117 from "./Components/pages/imoveis/IM80117";
-import IM80133 from "./Components/pages/imoveis/IM80133";
-import IM80017 from "./Components/pages/imoveis/IM80017";
-import IM80045 from "./Components/pages/imoveis/IM80045";
-import IM79981 from "./Components/pages/imoveis/IM79981";
 
 const App = () => {
     return (
@@ -30,13 +23,6 @@ const App = () => {
                         <Route path="contato" element={<Contato />} />
                         <Route path="missao" element={<Missao />} />
                         <Route path="imoveis" element={<Imoveis />} />
-                        <Route path="IM80058" element={<IM80058 />} />
-                        <Route path="IM80109" element={<IM80109 />} />
-                        <Route path="IM80117" element={<IM80117 />} />
-                        <Route path="IM80133" element={<IM80133 />} />
-                        <Route path="IM80017" element={<IM80017 />} />
-                        <Route path="IM80045" element={<IM80045 />} />
-                        <Route path="IM79981" element={<IM79981 />} />
                     </Routes>
                 </div>
             </div>
--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -21,15 +21,7 @@ export default function Imoveis() {
                         </thead>
                         <tbody>
                             <span id="insertion_table"></span>
-<tr id="IM80058"><th><Link to="/IM80058">IM80058</Link></th><th>Casa em Perdizes com 110m²</th><th>Aluguel</th><th>R$ 4.000,00</th><th>R$ 0,00</th></tr>
-<tr id="IM80109"><th><Link to="/IM80109">IM80109</Link></th><th>Loja em Santo Amaro com 140m²</th><th>Aluguel</th><th>R$ 24.000,00</th><th>R$ 0,00</th></tr>
-<tr id="IM80117"><th><Link to="/IM80117">IM80117</Link></th><th>Casa em Jardim Piracuama</th><th>Venda</th><th>R$ 0,00</th><th>R$ 250.000,00</th></tr>
-<tr id="IM80133"><th><Link to="/IM80133">IM80133</Link></th><th>Galpão em Ipiranga com 150m²</th><th>Aluguel</th><th>R$ 9.500,00</th><th>R$ 0,00</th></tr>
-<tr id="IM80017"><th><Link to="/IM80017">IM80017</Link></th><th>Casa em Cambuci com 180m²</th><th>Aluguel</th><th>R$ 6.300,00</th><th>R$ 0,00</th></tr>
-<tr id="IM80045"><th><Link to="/IM80045">IM80045</Link></th><th>Casa em Brooklin Paulista com 174m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.450.000,00</th></tr>
-                            <tr id="IM79981">
                                 <th>
-                                    <Link to="/IM79981">IM79981</Link>
                                 </th>
                                 <th>Casa em Vila da Saúde com 80m²</th>
                                 <th>Aluguel</th>
