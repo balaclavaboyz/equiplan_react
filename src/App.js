d5005148e9134d937cb472855714452dc7af167e
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-08-02 14:48:17 -0300

    add nova page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Missao from "./Components/pages/missao";
 import WhatsappIcon from "./Components/WhatsappIcon";
 import Footer from "./Components/Footer";
 import Imoveis from "./Components/pages/Imoveis";
+import IM80198 from "./Components/pages/imoveis/IM80198";
 import IM80221 from "./Components/pages/imoveis/IM80221";
 import IM80270 from "./Components/pages/imoveis/IM80270";
 import IM80301 from "./Components/pages/imoveis/IM80301";
@@ -27,6 +28,7 @@ const App = () => {
                         <Route path="contato" element={<Contato />} />
                         <Route path="missao" element={<Missao />} />
                         <Route path="imoveis" element={<Imoveis />} />
+                        <Route path="IM80198" element={<IM80198 />} />
                         <Route path="IM80221" element={<IM80221 />} />
                         <Route path="IM80270" element={<IM80270 />} />
                         <Route path="IM80301" element={<IM80301 />} />
--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -21,6 +21,7 @@ export default function Imoveis() {
                         </thead>
                         <tbody>
                             <span id="insertion_table"></span>
+<tr id="IM80198"><th><Link to="/IM80198">IM80198</Link></th><th>Casa em Jardim das Bandeiras com 200m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 2.600.000,00</th></tr>
 <tr id="IM80221"><th><Link to="/IM80221">IM80221</Link></th><th>Casa em Vila Mascote com 200m²</th><th>Venda e Aluguel</th><th>R$ 7.060,00</th><th>R$ 1.480.000,00</th></tr>
 <tr id="IM80270"><th><Link to="/IM80270">IM80270</Link></th><th>Casa em Vila Clementino com 280m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 3.500.000,00</th></tr>
 <tr id="IM80301"><th><Link to="/IM80301">IM80301</Link></th><th>Casa em Lapa com 343m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 2.190.000,00</th></tr>
