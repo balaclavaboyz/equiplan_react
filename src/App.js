51255d652e47d15c18297d5c2d022f6210638383
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2022-08-16 11:35:11 -0300

    add nova page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Missao from "./Components/pages/missao";
 import FloatingIcons from "./Components/WhatsappIcon";
 import Footer from "./Components/Footer";
 import Imoveis from "./Components/pages/Imoveis";
+import IM80442 from "./Components/pages/imoveis/IM80442";
 import IM79968 from "./Components/pages/imoveis/IM79968";
 import IM80045 from "./Components/pages/imoveis/IM80045";
 
@@ -25,6 +26,7 @@ const App = () => {
                         <Route path="contato" element={<Contato />} />
                         <Route path="missao" element={<Missao />} />
                         <Route path="imoveis" element={<Imoveis />} />
+                        <Route path="IM80442" element={<IM80442 />} />
                         <Route path="IM79968" element={<IM79968 />} />
                         <Route path="IM80045" element={<IM80045 />} />
                     </Routes>
--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -21,6 +21,7 @@ export default function Imoveis() {
                         </thead>
                         <tbody>
                             <span id="insertion_table"></span>
+<tr id="IM80442"><th><Link to="/IM80442">IM80442</Link></th><th>Casa em Santa Cecília</th><th>Venda e Aluguel</th><th>R$ 25.000,00</th><th>R$ 4.240.000,00</th></tr>
 <tr id="IM79968"><th><Link to="/IM79968">IM79968</Link></th><th>Casa em Vila Romana com 170m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.480.000,00</th></tr>
 <tr id="IM80045"><th><Link to="/IM80045">IM80045</Link></th><th>Casa em Brooklin Paulista com 174m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.450.000,00</th></tr>
                         </tbody>
