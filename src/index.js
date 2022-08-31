500b2b65deb57eea243ad81a96a3e71087b13149
peter <peterthomaskim@gmail.com>
2022-08-31 14:21:59 -0300

    GA setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import App from "./App.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import ReactGA from 'react-ga'
 
+ReactGA.initialize('UA-239549109-1')
+ReactGA.pageview(window.location.pathname + window.location.search)
 ReactDOM.render(
         <Router basename={process.env.PUBLIC_URL}>
             <App />
