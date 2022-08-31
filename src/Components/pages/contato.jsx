db37c4a11d835514f46972b3d2fb7dd5b5ed9229
peter <peterthomaskim@gmail.com>
2022-08-31 11:14:42 -0300

    tele and whatsapp changed

--- src/Components/pages/contato.jsx
+++ src/Components/pages/contato.jsx
@@ -9,9 +9,9 @@ const Faq = () => {
       <br></br>
       <Row>
         <h2>Escreva suas dúvidas</h2>
-        <p>Whatsapp: <a href="https://wa.me/5511973364612">(11)97336-4612</a></p>
+        <p>Whatsapp: <a href="https://wa.me/5511988109466">(11)98810-9466</a></p>
         <p>E-mail: <a href="mailto:equiplan@equiplan.com.br">equiplan@equiplan.com.br</a> </p>
-        <p>Contato: <a href="tel:11973364612">(11)97336-4612</a></p>
+        <p>Contato: <a href="tel:11988109466">(11)98810-9466</a></p>
 	<p>Endereço: <a href="rua aimores 101 - São Paulo">rua aimores 101 - São Paulo</a></p>
       </Row>
     </Container>
