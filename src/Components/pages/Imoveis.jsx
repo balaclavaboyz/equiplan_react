220a80dabe6a581a785206cc9deb9f9eb5402b43
peter <peterthomaskim@gmail.com>
2023-01-10 20:23:57 -0300

    css and html done for grid images in imoveis page

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import "../../assets/table.css";
 import "../../assets/table.js";
 import logo from '../../assets/galeria_2/2.png'
+import im80045 from '../../assets/imoveis/IM80045/0.jpg'
+import im80655 from '../../assets/imoveis/IM80655/0.jpg'
+import im80959 from '../../assets/imoveis/IM80959/0.jpg'
+import im81289 from '../../assets/imoveis/IM81289/0.jpg'
+import im81362 from '../../assets/imoveis/IM81362/0.jpg'
 
 export default function Imoveis() {
     return (
@@ -30,11 +35,11 @@ export default function Imoveis() {
                         </tbody>
                     </table> */}
                     <div className="grid">
-                        <div><a href="/im81289"><div className="inside_image">IM81289</div><img src={logo} alt="link imovel im81289" ></img></a></div>
-                        <div><a href="/im81362"><div className="inside_image">IM81362</div><img src={logo} alt="link imovel im81362" ></img></a></div>
-                        <div><a href="/im80959"><div className="inside_image">IM80959</div><img src={logo} alt="link imovel im80959" ></img></a></div>
-                        <div><a href="/im80655"><div className="inside_image">IM80655</div><img src={logo} alt="link imovel im80655" ></img></a></div>
-                        <div><a href="/im80045"><div className="inside_image">IM80045</div><img src={logo} alt="link imovel im80045" ></img></a></div>
+                        <div><a href="/im81289"><div className="inside_image">IM81289</div><img src={im80045} alt="link imovel im81289" ></img></a></div>
+                        <div><a href="/im81362"><div className="inside_image">IM81362</div><img src={im80655} alt="link imovel im81362" ></img></a></div>
+                        <div><a href="/im80959"><div className="inside_image">IM80959</div><img src={im80959} alt="link imovel im80959" ></img></a></div>
+                        <div><a href="/im80655"><div className="inside_image">IM80655</div><img src={im81289} alt="link imovel im80655" ></img></a></div>
+                        <div><a href="/im80045"><div className="inside_image">IM80045</div><img src={im81362} alt="link imovel im80045" ></img></a></div>
                     </div>
                 </div>
             </div>
