0085a7001685cb1e01f6ababac656c4bcb0e24f3
peter <peterthomaskim@gmail.com>
2023-01-10 16:49:15 -0300

    removed floating icons

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
                 </div>
             </div>
             <Footer />
-            <FloatingIcons />
+            {/* <FloatingIcons /> */}
         </>
     );
 };
