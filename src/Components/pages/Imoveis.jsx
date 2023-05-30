6762dec4ca4bd223df84df71c1288c1a555c8cd1
peter <peterthomaskim@gmail.com>
2023-05-30 17:19:48 -0300

    fixed wrong import

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import "../../assets/table.css";
 import "../../assets/table.js";
 import logo from '../../assets/galeria_2/2.png'
-import IM73832 from '../../assets/imoveis/IM73832/0.jpg'
-import IM83699 from '../../assets/imoveis/IM83699/0.jpg'
+import im73832 from '../../assets/imoveis/IM73832/0.jpg'
+import im83699 from '../../assets/imoveis/IM83699/0.png'
 import im80045 from '../../assets/imoveis/IM80045/0.jpg'
 import im80655 from '../../assets/imoveis/IM80655/0.jpg'
 import im80959 from '../../assets/imoveis/IM80959/0.jpg'
@@ -19,9 +19,9 @@ export default function Imoveis() {
                 <div className="indice_imoveis_wrapper">
                     <div className="grid">
                         <span id='insert'></span>
-<tr id="IM73832"><th><Link to="/IM73832">IM73832</Link></th><th>Armazém em Várzea da Barra Funda com 870m²</th><th>Venda e Aluguel</th><th>R$ 24.000,00</th><th>R$ 4.500.000,00</th></tr>
-<tr id="IM83699"><th><Link to="/IM83699">IM83699</Link></th><th>Galpão em Água Funda com 232m²</th><th>Aluguel</th><th>R$ 6.000,00</th><th>R$ 0,00</th></tr>
+                        <div><a href="/im73832"><div className="inside_image">IM73832</div><img src={im73832} alt="link imovel im73832" ></img></a></div>
                         <div><a href="/im80959"><div className="inside_image">IM80959</div><img src={im80959} alt="link imovel im80959" ></img></a></div>
+                        <div><a href="/im83699"><div className="inside_image">IM83699</div><img src={im83699} alt="link imovel im83699" ></img></a></div>
                         <div><a href="/im80655"><div className="inside_image">IM80655</div><img src={im80655} alt="link imovel im80655" ></img></a></div>
                         <div><a href="/im80045"><div className="inside_image">IM80045</div><img src={im80045} alt="link imovel im80045" ></img></a></div>
 						<div><a href="/im83609"><div className="inside_image">IM83609</div><img src={im83609} alt="link imovel im83609" ></img></a></div>
