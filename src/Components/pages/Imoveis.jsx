9c72210c9c149004cce2df14fb30cd85a8785dde
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2023-05-30 09:57:38 -0300

    add nova page

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -6,7 +6,6 @@ import logo from '../../assets/galeria_2/2.png'
 import im80045 from '../../assets/imoveis/IM80045/0.jpg'
 import im80655 from '../../assets/imoveis/IM80655/0.jpg'
 import im80959 from '../../assets/imoveis/IM80959/0.jpg'
-import im81289 from '../../assets/imoveis/IM81289/0.jpg'
 import im81362 from '../../assets/imoveis/IM81362/0.jpg'
 
 export default function Imoveis() {
@@ -26,7 +25,6 @@ export default function Imoveis() {
                         </thead>
                         <tbody>
                             <span id="insertion_table"></span>
-<tr id="IM81289"><th><Link to="/IM81289">IM81289</Link></th><th>Casa em Vila Parque Jabaquara com 570m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.400.000,00</th></tr>
 <tr id="IM81362"><th><Link to="/IM81362">IM81362</Link></th><th>Casa em Vila Cordeiro com 120m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.300.000,00</th></tr>
 <tr id="IM80959"><th><Link to="/IM80959">IM80959</Link></th><th>Prédio Comercial + Galpão em Jaraguá com 700m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.590.000,00</th></tr>
 <tr id="IM80655"><th><Link to="/IM80655">IM80655</Link></th><th>Casa em Alto da Lapa com 297m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 2.438.000,00</th></tr>
@@ -34,7 +32,6 @@ export default function Imoveis() {
                         </tbody>
                     </table> */}
                     <div className="grid">
-                        <div><a href="/im81289"><div className="inside_image">IM81289</div><img src={im81289} alt="link imovel im81289" ></img></a></div>
                         <div><a href="/im81362"><div className="inside_image">IM81362</div><img src={im81362} alt="link imovel im81362" ></img></a></div>
                         <div><a href="/im80959"><div className="inside_image">IM80959</div><img src={im80959} alt="link imovel im80959" ></img></a></div>
                         <div><a href="/im80655"><div className="inside_image">IM80655</div><img src={im80655} alt="link imovel im80655" ></img></a></div>
