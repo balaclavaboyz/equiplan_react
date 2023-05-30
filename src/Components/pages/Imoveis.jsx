7d1635a03055cadf8c096db25862dc7526ba1908
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2023-05-30 10:09:06 -0300

    add nova page

--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -25,6 +25,7 @@ export default function Imoveis() {
                         </thead>
                         <tbody>
                             <span id="insertion_table"></span>
+<tr id="IM83609"><th><Link to="/IM83609">IM83609</Link></th><th>Galpão em Água Branca com 657m²</th><th>Venda e Aluguel</th><th>R$ 25.000,00</th><th>R$ 4.800.000,00</th></tr>
 <tr id="IM81362"><th><Link to="/IM81362">IM81362</Link></th><th>Casa em Vila Cordeiro com 120m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.300.000,00</th></tr>
 <tr id="IM80959"><th><Link to="/IM80959">IM80959</Link></th><th>Prédio Comercial + Galpão em Jaraguá com 700m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 1.590.000,00</th></tr>
 <tr id="IM80655"><th><Link to="/IM80655">IM80655</Link></th><th>Casa em Alto da Lapa com 297m²</th><th>Venda</th><th>R$ 0,00</th><th>R$ 2.438.000,00</th></tr>
