5e4ec27c9bd0ce2cd933abdee13cd520f5c84a98
Peter <33288116+balaclavaboyz@users.noreply.github.com>
2023-05-31 13:27:45 -0300

    add nova page

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ import IM81709 from "./Components/pages/imoveis/IM81709";
 import IM73832 from "./Components/pages/imoveis/IM73832";
 import IM83699 from "./Components/pages/imoveis/IM83699";
 import IM83609 from "./Components/pages/imoveis/IM83609";
-import IM81362 from "./Components/pages/imoveis/IM81362";
 import IM80959 from "./Components/pages/imoveis/IM80959";
-import IM80655 from "./Components/pages/imoveis/IM80655";
-import IM80045 from "./Components/pages/imoveis/IM80045";
 import Manu from './Components/Manu'
 
 const App = () => {
@@ -36,14 +33,8 @@ const App = () => {
                         <Route path="IM73832" element={<IM73832 />} />
                         <Route path="IM83699" element={<IM83699 />} />
                         <Route path="IM83609" element={<IM83609 />} />
-                        <Route path="IM81362" element={<IM81362 />} />
                         <Route path="IM80959" element={<IM80959 />} />
-                        <Route path="IM80045" element={<IM80045 />} />
-                        <Route path="IM80655" element={<IM80655></IM80655>}></Route>
-                        <Route path='1' element={<IM80045 />}></Route>
-                        <Route path="2" element={<IM80655 />} />
                         <Route path='3' element={<IM80959></IM80959>}></Route>
-                        <Route path="5" element={<IM81362 />} />
                     </Routes>
                 </div>
             </div>
--- src/Components/pages/Imoveis.jsx
+++ src/Components/pages/Imoveis.jsx
@@ -6,10 +6,7 @@ import logo from '../../assets/galeria_2/2.png'
 import IM81709 from '../../assets/imoveis/IM81709/0.jpg'
 import im73832 from '../../assets/imoveis/IM73832/0.jpg'
 import im83699 from '../../assets/imoveis/IM83699/0.png'
-import im80045 from '../../assets/imoveis/IM80045/0.jpg'
-import im80655 from '../../assets/imoveis/IM80655/0.jpg'
 import im80959 from '../../assets/imoveis/IM80959/0.jpg'
-import im81362 from '../../assets/imoveis/IM81362/0.jpg'
 import im83609 from '../../assets/imoveis/IM83609/0.jpg'
 import './span.css'
 
@@ -24,10 +21,7 @@ export default function Imoveis() {
                         <div><a href="/im73832"><div className="inside_image">IM73832</div><img src={im73832} alt="link imovel im73832" ></img></a></div>
                         <div><a href="/im80959"><div className="inside_image">IM80959</div><img src={im80959} alt="link imovel im80959" ></img></a></div>
                         <div><a href="/im83699"><div className="inside_image">IM83699</div><img src={im83699} alt="link imovel im83699" ></img></a></div>
-                        <div><a href="/im80655"><div className="inside_image">IM80655</div><img src={im80655} alt="link imovel im80655" ></img></a></div>
-                        <div><a href="/im80045"><div className="inside_image">IM80045</div><img src={im80045} alt="link imovel im80045" ></img></a></div>
 						<div><a href="/im83609"><div className="inside_image">IM83609</div><img src={im83609} alt="link imovel im83609" ></img></a></div>
-                        <div><a href="/im81362"><div className="inside_image">IM81362</div><img src={im81362} alt="link imovel im81362" ></img></a></div>
                     </div>
                 </div>
             </div>
